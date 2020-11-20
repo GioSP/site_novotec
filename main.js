fe19edc3bfd8d0a79789653601965c6dd17ceeaf
GiovanaS <GiovanaSouPereira@hotmail.com>
2020-11-20 18:04:49 -0300

    ultima versao da aula

---
+++ main.js
@@ -0,0 +1,29 @@
+function botao(){
+    document.getElementById("agradecimento").innerHTML = "<b>Obrigado por clicar</b>";
+    //console.log(document.getElementById("agradecimento"));
+    //alert("Obrigado por clicar");
+}
+
+function redirecionar(){
+    window.open("https://github.com/GioSP/projeto-curso-git")
+    //window.location.href = "https://github.com/GioSP/projeto-curso-git";
+}
+
+function trocar(elemento){
+    //document.getElementById("mousemove").innerHTML = "Obrigado por passar o mouse";
+    elemento.innerHTML = "Obrigado por passar o mouse";
+    //alert("trocar texto");
+}
+
+function voltar(elemento){
+    //document.getElementById("mousemove").innerHTML = "Passe o mouse aqui";
+    elemento.innerHTML = "Passe o mouse aqui";
+}
+
+function load(){
+    alert("pagina carregada");
+}
+
+function funcaoChange(elemento){
+    console.log(elemento.value)
+}
